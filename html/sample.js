126cb29ae043284a79571c0efee025f2880e00b7
Matsumoto <rxn302n1@yahoo.co.jp>
2017-09-07 15:33:19 +0900

    分割

---
+++ html/sample.js
@@ -0,0 +1,10 @@
+$(function() {
+     $("#profile01").load("./profile01.html");
+     $("#profile02").load("./profile02.html");
+     $("#profile03").load("profile03.html");
+     $("#profile04").load("profile04.html");
+     $("#header").load("header.html");
+     $("#side").load("side.html");
+     $("#footer").load("footer.html");
+    $("#day2").load("day2.html");
+});
